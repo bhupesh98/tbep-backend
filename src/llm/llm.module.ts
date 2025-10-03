1c1f240cd95efbcb76c7456dab693849192d9927
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2025-10-04 04:30:59 +0530

    chore(deps): dependency update

--- src/llm/llm.module.ts
+++ src/llm/llm.module.ts
@@ -1,21 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import * as fs from 'fs';
+import { Module } from '@nestjs/common';
 import { LlmController } from './llm.controller';
 import { LlmService } from './llm.service';
-import { NextFunction, Request, Response } from 'express';
 
 @Module({
   controllers: [LlmController],
   providers: [LlmService],
 })
-export class LlmModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    const logStream = fs.createWriteStream('chat.log', { flags: 'a' });
-    consumer
-      .apply((req: Request, _res: Response, next: NextFunction) => {
-        logStream.write(`[${new Date().toISOString()}] ${req.body.question}\n`);
-        next();
-      })
-      .forRoutes('/llm/chat');
-  }
-}
+export class LlmModule {}
